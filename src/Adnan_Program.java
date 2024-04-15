bbd24d887344a719a3989a99240ed36d4cb9d932
AdnanSKK <s.aamiradnan1@gmail.com>
2024-04-15 19:07:43 +0530

    Initial commit

---
+++ src/Adnan_Program.java
@@ -0,0 +1,13 @@
+public class Adnan_Program {
+    public static void main(String[] args) {
+        System.out.println("hi adnan");
+        System.out.println("hi aamir");
+        System.out.println(1/1);
+        System.out.println(1/1);
+
+
+    }
+
+
+
+}
---
+++ src/Ex_11042024/LAB001.java
@@ -0,0 +1,7 @@
+package Ex_11042024;
+
+public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
