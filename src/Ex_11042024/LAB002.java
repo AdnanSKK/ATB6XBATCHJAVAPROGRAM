5d3ae3a31df9f892a5662a9cb827ce5d9c9045eb
AdnanSKK <s.aamiradnan1@gmail.com>
2024-04-16 01:24:35 +0530

    Initial Commit

--- src/Ex_11042024/LAB002.java
+++ src/Ex_11042024/LAB002.java
@@ -1,2 +1,13 @@
-package Ex_11042024;public class LAB002 {
+package Ex_11042024;
+
+public class LAB002 {
+    public static void main(String[] args) {
+        System.out.println("hfjfhfkkhljljlj");
+
+        int age = 65;
+        age=-97;
+        age = age+97;
+        System.out.println(age);
+    }
+
 }
---
+++ src/Ex_13042024/Multiplication_table_using_printf.java
@@ -0,0 +1,14 @@
+package Ex_13042024;
+
+public class Multiplication_table_using_printf {
+
+        public static void main(String[] args) {
+            int number = 9;
+            System.out.printf("Multiplication table of %d number is : \n",number);
+            for (int i = 1; i <=10 ; i++) {
+                System.out.printf("%d x %d = %d\n",number,i,(number*i));
+
+            }
+        }
+    }
+
