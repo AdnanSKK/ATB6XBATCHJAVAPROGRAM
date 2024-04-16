b3a29053f4d127d2ba10c9cf0a73a1e8cd4c953f
AdnanSKK <s.aamiradnan1@gmail.com>
2024-04-16 18:34:55 +0530

    Initial Commit

--- src/Ex_11042024/LAB002.java
+++ src/Ex_11042024/LAB002.java
@@ -2,11 +2,11 @@ package Ex_11042024;
 
 public class LAB002 {
     public static void main(String[] args) {
-        System.out.println("hfjfhfkkhljljlj");
+        System.out.println("hfjfhfkkhljlklhjghljlj");
 
         int age = 65;
-        age=-97;
-        age = age+97;
+        age=-99;
+        age = age+99;
         System.out.println(age);
     }
 
