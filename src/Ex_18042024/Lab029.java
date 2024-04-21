647ca14eb3bba1f5b6215f6de633b6e09f3366e7
AdnanSKK <s.aamiradnan1@gmail.com>
2024-04-21 20:29:14 +0530

    Initial Commit

---
+++ src/Ex_18042024/Lab029.java
@@ -0,0 +1,9 @@
+package Ex_18042024;
+
+public class Lab029 {
+    public static void main(String[] args) {
+int a =19;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+    }
+}
