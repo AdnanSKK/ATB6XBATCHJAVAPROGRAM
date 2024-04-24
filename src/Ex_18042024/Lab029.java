50dacd6e4e8b03457fc0ea9cfbe12e3d5dfa0908
AdnanSKK <s.aamiradnan1@gmail.com>
2024-04-25 00:06:40 +0530

    Initial Commit

--- src/Ex_18042024/Lab029.java
+++ src/Ex_18042024/Lab029.java
@@ -2,7 +2,7 @@ package Ex_18042024;
 
 public class Lab029 {
     public static void main(String[] args) {
-int a =19;
+int a =171;
         System.out.println(++a + a++ + a++);
         System.out.println(a);
     }
---
+++ src/Ex_20042024/Lab050.java
@@ -0,0 +1,13 @@
+package Ex_20042024;
+
+public class Lab050 {
+    public static void main(String[] args) {
+        String name1 = "Adnan";
+        String name2 = new String("adnan");
+
+        System.out.println(name2.toLowerCase());
+        System.out.println(name2.toUpperCase());
+
+
+    }
+}
