a9dfbdba7105c6d8f2e5e66519cb7c93abc66c85
AdnanSKK <s.aamiradnan1@gmail.com>
2024-05-06 15:40:15 +0530

    Initial Commit

---
+++ src/April/Ex_23042024/Lab069.java
@@ -0,0 +1,23 @@
+package April.Ex_23042024;
+
+public class Lab069 {
+    public static void main(String[] args) {
+
+        // age > 18 you can watch the movie
+        // You can't wathc the movie
+
+//        if(condition -> boolean true or false) // This is keyword
+//        {}
+//        else{
+//
+//        }
+
+        int age = 133;
+        if (age > 18){
+            System.out.println("You can watch the Movie A");
+        }else{
+            System.out.println("You can't");
+        }
+
+    }
+}
